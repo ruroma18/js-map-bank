4c5aae5d2ca6f68edf0d873496caa8fc8616ea4a
Roman Rudenko <ruroma18@gmail.com>
2022-04-20 16:34:50 +0300

    Rafactoring calculateTotalSum

--- assets/js/index.js
+++ assets/js/index.js
@@ -85,19 +85,19 @@ const { clientLevels: { junior },
 
 
 const connectionLevelsBank1 = new Map();
-connectionLevelsBank1.set('basic', basic.discount)
-  .set('pro', pro.discount)
-  .set('platinum', platinum.discount);
+connectionLevelsBank1.set('basic', basic)
+  .set('pro', pro)
+  .set('platinum', platinum);
 
 const connectionLevelsBank2 = new Map();
-connectionLevelsBank2.set('bronze', bronze.discount)
-  .set('silver', silver.discount)
-  .set('gold', gold.discount);
+connectionLevelsBank2.set('bronze', bronze)
+  .set('silver', silver)
+  .set('gold', gold);
 
 const connectionLevelsBank3 = new Map();
-connectionLevelsBank3.set('junior', junior.discount)
-  .set('middle', middle.discount)
-  .set('senior', senior.discount);
+connectionLevelsBank3.set('junior', junior)
+  .set('middle', middle)
+  .set('senior', senior);
 
 
 
@@ -109,23 +109,30 @@ connectionBankWithLevels.set('Zashad', connectionLevelsBank1)
 
 
 const calculateTotalSum = (user, basicSum) => {
-  let totalSum = 0;
+  const userConnectionBankLevel = connectionBankWithLevels.get(user.bankName);
+  if (userConnectionBankLevel.has(user.clientLevel) && userConnectionBankLevel.get(user.clientLevel) != undefined) {
+    const totalSum = basicSum - (basicSum * userConnectionBankLevel.get(user.clientLevel).discount);
+    user.availableMoney = user.availableMoney - totalSum;
+    return totalSum;
+  } else {
+    user.availableMoney = user.availableMoney - basicSum;
+    return basicSum;
+  }
+};
+
+const checkBeforeCalculating = () => {
   if (connectionBankWithLevels.has(user.bankName)) {
     if (basicSum > user.availableMoney) {
       const lackSum = basicSum - user.availableMoney;
       throw new RangeError(`You lack ${lackSum} for purchase`);
     } else {
-      const userConnectionBankLevel = connectionBankWithLevels.get(user.bankName);
-      if (userConnectionBankLevel.has(user.clientLevel) && userConnectionBankLevel.get(user.clientLevel) != undefined) {
-        totalSum = basicSum - (basicSum * userConnectionBankLevel.get(user.clientLevel));
-        user.availableMoney = user.availableMoney - totalSum;
-        return totalSum;
-      } else {
-        user.availableMoney = user.availableMoney - basicSum;
-        return basicSum;
-      };
+      calculateTotalSum(user, basicSum)
     };
   } else {
     throw new Error(`Bank doesn't exist!`);
-  };
-};
+  }
+}
+
+
+
+
